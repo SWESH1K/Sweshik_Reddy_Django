6fb9377642490e82ca79e9668bc64ce0abdcbe26
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-07-27 13:05:43 +0530

    Experiment 12 added

--- ClassFiles/MyFirstDjango/calc/models.py
+++ ClassFiles/MyFirstDjango/calc/models.py
@@ -29,3 +29,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out of Delivery', 'Out of Delivery'),
+        ('Delivered', 'Delivered')
+    )
+
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=300, choices=STATUS)
+
+    def __str__(self):
+        return f"Customer: {self.customer} --> Product: {self.product} ({self.status})"
--- ClassFiles/MyFirstDjango/calc/views.py
+++ ClassFiles/MyFirstDjango/calc/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 # Create your views here.
 def home(request):
@@ -13,8 +14,55 @@ def add(request):
 
 def dashboard(request):
     customers = Customer.objects.all()
-    return render(request, 'dashboard.html', {'customers':customers})
+    orders = Order.objects.all()
+    return render(request, 'dashboard.html', {'customers':customers, 'orders': orders})
 
 def products(request):
     products = Product.objects.all()
     return render(request, 'product.html', {'products':products})
+
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    customers = Customer.objects.all()
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {'customers':customers, 'cust':customer, 'orders':orders, 'ordcount':order_count}
+    return render(request, 'customer.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+
+    if request.method == "POST":
+        print("POST request received")
+        print("Form data:", request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            print("Form is valid")
+            form.save()
+            return redirect('/dashboard')
+        else:
+            print("Form is not valid")
+            print(form.errors)
+        
+    context = {'form': form}
+    return render(request, 'order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(request.POST, instance=order)
+    if form.is_valid():
+        form.save()
+        return redirect('/dashboard')
+    
+    context = {'form': form}
+
+    return render(request, 'order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/dashboard')
+    
+    context = {'item':order}
+    return render(request, 'delete.html', context)
